13aa04e4900aecb7fb0cc11eae05bea83af21d23
Héctor Caballero <hectorjosue.caballero@gmail.com>
2018-12-10 14:38:27 -0600

    Modificaciones_GUI_2_Beta
    Terminadas las modificaciones de la interfaz.

--- admin/views/view.cajero.php
+++ admin/views/view.cajero.php
@@ -4,79 +4,16 @@
         <div class="row mt-5">
           <!-- barra de navegacion  -->
           <div class="col-md-3" id="blue">
-            <button type="button" name="registro" class="btn_active" id="btn_registro" onclick="toggle_Registro()">
-              Registro de movimiento de productos
-            </button>
-            <button type="button" name="facturacion" class="" id="btn_facturacion" onclick="toggle_Facturacion()">
+            <button type="button" name="facturacion" class="btn_active" id="btn_facturacion" onclick="toggle_Facturacion()">
               Facturación
             </button>
           </div>
           <!-- panel -->
           <div class="col-md-9" id="white">
-            <!-- REGISTRO DE MOVIMIENTOS  -->
-            <div class="mt-5" id="div_registro">
-              <!-- Buscador -->
-              <form class="" action="" method="post">
-                <input type="text" name="" value="" placeholder="Buscar Producto">
-              </form>
-              <!-- Filtro  -->
-              <form class="" action="" method="post">
-                <div class="dates">
-                  <input id="date" type="date"><span>-</span>
-                  <input id="date" type="date">
-                </div>
-                <div class="radio d-flex justify-content-around">
-                  <div class="item_radio">
-                    <input type="radio" name="tiempo" value="hoy"> Solo hoy
-                  </div>
-                  <div class="item_radio">
-                    <input type="radio" name="tiempo" value="dia_concreto"> Día en concreto
-                  </div>
-                  <div class="item_radio">
-                    <input type="radio" name="tiempo" value="rango_fechas"> Rango de fechas
-                  </div>
-                </div>
-              <!-- Tabla -->
-              <table class="table table-striped mt-3">
-                <thead>
-                  <tr>
-                    <th scope="col">Producto</th>
-                    <th scope="col">Cantidad Vendidos</th>
-                    <th scope="col">Existencias</th>
-                    <th scope="col">Fecha</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>40</td>
-                    <td>dd/mm/yyyy</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>25</td>
-                    <td>dd/mm/yyyy</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>15</td>
-                    <td>dd/mm/yyyy</td>
-                  </tr>
-                </tbody>
-              </table>
-              <!-- Botones -->
-                <div class="botones d-flex justify-content-end">
-                  <button type="button" name="reiniciar" id="limpiar">Reiniciar</button>
-                  <input type="submit" name="generar" value="Generar">
-                </div>
-              </form>
-            </div>
             <!-- PANEL DE FACTURACION -->
             <div class="mt-5" id="div_facturacion">
               <div id="datos_generales">
+                <!-- TEXTO FIJO  -->
                 <div class="" id="texto">
                   <p>Tgus café</p>
                   <p>Waldina Paz</p>
@@ -85,31 +22,60 @@
                   <p class="mt-5">RTN: 08011966147855</p>
                   <p>CIA: 1A3F6 - 56A10B - BB4684 - A69C4F - FA45E5 -75</p>
                 </div>
+                <!-- CAMPOS VARIABLES  -->
                 <form class="mt-5" action="" method="post">
-                <div class="row">
-                    <div class="col-md-6">
-                      <p>N° de factutra: <span>00045</span></p>
-                      <p>Cajero: <span>Fulano de tal</span></p>
-                        <label for="cliente">Cliente: </label>
-                        <input type="text" name="cliente" id="cliente" value="">
-                    </div>
-                    <div class="col-md-6">
-                      <p>Fecha de emision: <span>DD/MM/YYYY</span></p>
-                      <label for="forma_pago">Forma de Pago:</label>
-                      <select name="forma_pago" id="forma_pago">
-                        <option value="efectivo">Efectivo</option>
-                        <option value="credito">Credito</option>
-                        <option value="debito" selected>Debito</option>
-                      </select>
-                    </div>
+                <!-- N° factura y Fecha emision -->
+                <div class="d-flex flex-row">
+                  <label for="numero_factura" class="col-2 col-form-label">N° de Factura:</label>
+                  <div class="col-4">
+                    <input class="form-control" type="text" id="numero_factura" disabled>
+                  </div>
+                  <label for="numero_factura" class="col-2 col-form-label">Fecha de emisión: </label>
+                  <div class="col-4">
+                    <input class="form-control" type="text" id="numero_factura" disabled>
+                  </div>
                 </div>
-                <div class="row">
+                <!-- Nombre cajero y forma de pago  -->
+                <div class="d-flex flex-row">
+                  <label for="cajero" class="col-2 col-form-label">Cajero:</label>
+                  <div class="col-4">
+                    <input class="form-control" type="text" id="cajero" name="cajero" value="Jorge" disabled>
+                  </div>
+                  <label for="forma_pago" class="col-2 col-form-label">Forma de pago: </label>
+                  <select name="forma_pago" id="forma_pago" class="custom-select col-4 mt-2" required>
+                    <option value="" disabled selected>Seleccione forma de pago</option>
+                    <option value="formal">Efectivo</option>
+                    <option value="entrevista">Debito</option>
+                    <option value="birthday" selected>Credito</option>
+                  </select>
+                </div>
+                <!-- Producto y Cantidad  -->
+                <div class="d-flex flex-row">
+                  <label for="slct_producto" class="col-2 col-form-label">Seleccione producto: </label>
+                  <select name="slct_producto" id="slct_producto" class="custom-select col-4 mt-2" required>
+                    <option value="" disabled selected>Seleccione producto de la lista</option>
+                    <option value="formal">Citrolax</option>
+                    <option value="entrevista">Latte</option>
+                    <option value="birthday" selected>Moccha</option>
+                  </select>
+                  <label for="cantidad" class="col-2 col-form-label">Cantidad: </label>
+                  <input  class="form-control col-4" type="number" name="cantidad" id="cantidad" min="1" max="1000" required>
+                </div>
+                <!-- Nombre Cliente -->
+                <div class="d-flex justify-content-between">
+                  <label for="nombre_cliente" class="col-2 col-form-label">Nombre cliente(Opcional):</label>
+                  <div class="col-4">
+                    <input class="form-control" type="text" id="nombre_cliente" name="nombre_cliente" value="">
+                  </div>
+                  <button class="btn btn-primary col-5" type="button" name="button">Agregar Producto</button>
+                </div>
+                <!-- Tabla -->
+                <div class="row mt-5">
                   <div class="col-md-12">
                     <table class="table table-striped">
                       <thead>
                         <tr>
-                          <th scope="col">Codigo</th>
-                          <th scope="col">Descripcion</th>
+                          <th scope="col">Producto</th>
                           <th scope="col">Precio Unitario</th>
                           <th scope="col">Cantidad</th>
                           <th scope="col">Total</th>
@@ -117,22 +83,19 @@
                       </thead>
                       <tbody>
                         <tr>
-                          <th scope="row">1</th>
-                          <td>Mark</td>
+                          <th scope="row">Citrolax</th>
                           <td>algo</td>
                           <td>5</td>
                           <td>100</td>
                         </tr>
                         <tr>
-                          <th scope="row">2</th>
-                          <td>Jacob</td>
+                          <th scope="row">Patate</th>
                           <td>algo</td>
                           <td>3</td>
                           <td>100</td>
                         </tr>
                         <tr>
-                          <th scope="row">3</th>
-                          <td>Larry</td>
+                          <th scope="row">Matate</th>
                           <td>algo</td>
                           <td>10</td>
                           <td>100</td>
@@ -141,13 +104,14 @@
                     </table>
                   </div>
                 </div>
+                <!-- Totales -->
                 <div class="row" id="sub_totales">
                   <div class="col-md-12">
                     <p>Sub-Total: <span>000000 L.</span></p>
                     <p>ISV 15%: <span>000000 L.</span></p>
                     <p>Total a pagar: <span>000000 L.</span></p>
-
-                    <button type="button" name="Imprimir">Imprimir</button>
+                    <!-- Botones -->
+                    <button type="submit" name="Imprimir">Imprimir</button>
                   </div>
                 </div>
                 </form>
